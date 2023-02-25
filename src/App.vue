fa7526380b830a49dcf87eda715acd0d5395a706
JuanraCM <juanracm.dev@gmail.com>
2023-02-25 12:59:46 +0000

    AppHeader & AppFooter components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="container">
-    <header>
-      There goes a header!
-    </header>
+  <div class="container m-auto">
+    <AppHeader />
 
     <main>
       <ImageCanvas />
     </main>
+
+    <AppFooter />
   </div>
 </template>
 
 <script setup>
+import AppHeader from './components/AppHeader.vue'
 import ImageCanvas from './components/ImageCanvas.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header class="text-center p-20">
+    <p class="text-4xl font-semibold">Icon to wallpaper</p>
+  </header>
+</template>
